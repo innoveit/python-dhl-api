70c3c9a44dfc268953a8e8f22d5d6bdf29f707f6
Matteo Munaretto <matteo.munaretto@innove.it>
2025-04-28 17:08:51 +0200

    Formatting

--- python_dhl/resources/shipment.py
+++ python_dhl/resources/shipment.py
@@ -11,7 +11,7 @@ class DHLProduct:
             "dimensions": {
                 "length": self.length,
                 "width": self.width,
-                "height": self.height
+                "height": self.height,
             },
         }
 
@@ -30,8 +30,8 @@ class DHLAccountType:
 
     def to_dict(self):
         dict = {
-            'typeCode': self.type_code.value,
-            'number': self.number,
+            "typeCode": self.type_code.value,
+            "number": self.number,
         }
         return dict
 
@@ -47,7 +47,10 @@ class DHLAddedService:
     "DD" (DTP service) commonly referred to as Duties and Taxes Paid (DTP) indicates that the shipper or account holder incurs the full costs of the shipment, including any duties or taxes that might be incurred from customs.
     "DU" (IOR service) the official importer of the shipment, meaning that duties, taxes, or any other payments will go through the IOR, but the end destination of the shipment will go to the receiver, who doesn’t have to pay for a thing.
     """
-    def __init__(self, service_code, value=None, currency=None, method=None, dangerous_goods=None):
+
+    def __init__(
+        self, service_code, value=None, currency=None, method=None, dangerous_goods=None
+    ):
         self.service_code = service_code
         self.value = value
         self.currency = currency
@@ -56,22 +59,31 @@ class DHLAddedService:
 
     def to_dict(self):
         dict = {
-            'serviceCode': self.service_code,
+            "serviceCode": self.service_code,
         }
         if self.value:
-            dict['value'] = self.value
+            dict["value"] = self.value
         if self.currency:
-            dict['currency'] = self.currency
+            dict["currency"] = self.currency
         if self.currency:
-            dict['method'] = self.method
+            dict["method"] = self.method
         if self.currency:
-            dict['dangerousGoods'] = self.dangerous_goods
+            dict["dangerousGoods"] = self.dangerous_goods
         return dict
 
 
 class DHLShipmentContent:
-    def __init__(self, packages, is_custom_declarable, description, incoterm_code, unit_of_measurement,
-                 declared_value=None, declared_value_currency=None, product_code=None):
+    def __init__(
+        self,
+        packages,
+        is_custom_declarable,
+        description,
+        incoterm_code,
+        unit_of_measurement,
+        declared_value=None,
+        declared_value_currency=None,
+        product_code=None,
+    ):
         self.packages = packages
         self.is_custom_declarable = is_custom_declarable
         self.description = description
@@ -86,16 +98,16 @@ class DHLShipmentContent:
         for p in self.packages:
             packages.append(p.to_dict())
         dict = {
-            'packages': packages,
-            'isCustomsDeclarable': self.is_custom_declarable,
-            'description': self.description,
-            'incoterm': self.incoterm_code,
-            'unitOfMeasurement': self.unit_of_measurement,
+            "packages": packages,
+            "isCustomsDeclarable": self.is_custom_declarable,
+            "description": self.description,
+            "incoterm": self.incoterm_code,
+            "unitOfMeasurement": self.unit_of_measurement,
         }
         if self.declared_value:
-            dict['declaredValue'] = self.declared_value
+            dict["declaredValue"] = self.declared_value
         if self.declared_value_currency:
-            dict['declaredValueCurrency'] = self.declared_value_currency
+            dict["declaredValueCurrency"] = self.declared_value_currency
         return dict
 
     def to_dict_pickup(self):
@@ -103,43 +115,55 @@ class DHLShipmentContent:
         for p in self.packages:
             packages.append(p.to_dict())
         dict = {
-            'productCode': self.product_code,
-            'packages': packages,
-            'isCustomsDeclarable': self.is_custom_declarable,
-            'unitOfMeasurement': self.unit_of_measurement,
+            "productCode": self.product_code,
+            "packages": packages,
+            "isCustomsDeclarable": self.is_custom_declarable,
+            "unitOfMeasurement": self.unit_of_measurement,
         }
         if self.declared_value:
-            dict['declaredValue'] = self.declared_value
+            dict["declaredValue"] = self.declared_value
         if self.declared_value_currency:
-            dict['declaredValueCurrency'] = self.declared_value_currency
+            dict["declaredValueCurrency"] = self.declared_value_currency
         return dict
 
 
 class DHLShipmentOutput:
-    def __init__(self, dpi, logo_file_format, logo_file_base64, encoding_format='pdf',
-                 split_transport_and_waybill_doc_labels=True,
-                 all_documents_in_one_image=True, split_documents_by_pages=True, split_invoice_and_receipt=True):
+    def __init__(
+        self,
+        dpi,
+        logo_file_format,
+        logo_file_base64,
+        encoding_format="pdf",
+        split_transport_and_waybill_doc_labels=True,
+        all_documents_in_one_image=True,
+        split_documents_by_pages=True,
+        split_invoice_and_receipt=True,
+    ):
         self.dpi = dpi
         self.logo_file_format = logo_file_format
         self.logo_file_base64 = logo_file_base64
         self.encoding_format = encoding_format
-        self.split_transport_and_waybill_doc_labels = split_transport_and_waybill_doc_labels
+        self.split_transport_and_waybill_doc_labels = (
+            split_transport_and_waybill_doc_labels
+        )
         self.all_documents_in_one_image = all_documents_in_one_image
         self.split_documents_by_pages = split_documents_by_pages
         self.split_invoice_and_receipt = split_invoice_and_receipt
 
     def to_dict(self):
         return {
-            'printerDPI': self.dpi,
-            'customerLogos': [{
-                'fileFormat': self.logo_file_format.upper(),
-                'content': self.logo_file_base64
-            }],
-            'encodingFormat': self.encoding_format.lower(),
-            'splitTransportAndWaybillDocLabels': self.split_transport_and_waybill_doc_labels,
-            'allDocumentsInOneImage': self.all_documents_in_one_image,
-            'splitDocumentsByPages': self.split_documents_by_pages,
-            'splitInvoiceAndReceipt': self.split_invoice_and_receipt,
+            "printerDPI": self.dpi,
+            "customerLogos": [
+                {
+                    "fileFormat": self.logo_file_format.upper(),
+                    "content": self.logo_file_base64,
+                }
+            ],
+            "encodingFormat": self.encoding_format.lower(),
+            "splitTransportAndWaybillDocLabels": self.split_transport_and_waybill_doc_labels,
+            "allDocumentsInOneImage": self.all_documents_in_one_image,
+            "splitDocumentsByPages": self.split_documents_by_pages,
+            "splitInvoiceAndReceipt": self.split_invoice_and_receipt,
         }
 
 
@@ -160,10 +184,25 @@ class DHLShipment:
     at a specific time or if the package is brought to a DHL service point / office.
     If false it is not necessary to call the pickup service.
     """
-    def __init__(self, sender_contact, sender_address, receiver_contact, receiver_address, ship_datetime,
-                 product_code, added_services, content, output_format, accounts,
-                 customer_references=None, sender_registration_numbers=None,
-                 request_pickup=False, pickup_close_time=None, pickup_location=None):
+
+    def __init__(
+        self,
+        sender_contact,
+        sender_address,
+        receiver_contact,
+        receiver_address,
+        ship_datetime,
+        product_code,
+        added_services,
+        content,
+        output_format,
+        accounts,
+        customer_references=None,
+        sender_registration_numbers=None,
+        request_pickup=False,
+        pickup_close_time=None,
+        pickup_location=None,
+    ):
         self.sender_contact = sender_contact
         self.sender_address = sender_address
         self.sender_registration_numbers = sender_registration_numbers
@@ -182,8 +221,17 @@ class DHLShipment:
 
 
 class DHLPickup:
-    def __init__(self, sender_contact, sender_address, receiver_contact, receiver_address, pickup_datetime,
-                 content, accounts, sender_registration_numbers=None):
+    def __init__(
+        self,
+        sender_contact,
+        sender_address,
+        receiver_contact,
+        receiver_address,
+        pickup_datetime,
+        content,
+        accounts,
+        sender_registration_numbers=None,
+    ):
         self.sender_contact = sender_contact
         self.sender_address = sender_address
         self.sender_registration_numbers = sender_registration_numbers
@@ -202,14 +250,21 @@ class DHLDocumentImage:
 
     def to_dict(self):
         return {
-            'typeCode': self.type_code,
-            'imageFormat': self.image_format.upper(),
-            'content': self.content
+            "typeCode": self.type_code,
+            "imageFormat": self.image_format.upper(),
+            "content": self.content,
         }
 
 
 class DHLDocument:
-    def __init__(self, tracking_number, original_planned_shipping_date, product_code, document_images, accounts):
+    def __init__(
+        self,
+        tracking_number,
+        original_planned_shipping_date,
+        product_code,
+        document_images,
+        accounts,
+    ):
         self.tracking_number = tracking_number
         self.original_planned_shipping_date = original_planned_shipping_date
         self.product_code = product_code
